400b03f26538090f5955ce033ff0e767ede99887
Prachi Bala <prachibala330@gmail.com>
2022-10-28 20:41:19 +0600

    ver-1.2

--- src/Pages/PrivateRoute.js
+++ src/Pages/PrivateRoute.js
@@ -2,11 +2,12 @@ import React from "react";
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../Contexts/UserContext";
-const PrivateRoute = () => {
+const PrivateRoute = ({ children }) => {
     const { user } = useState(AuthContext);
     if (!user) {
         return <Navigate to="/login"></Navigate>;
     }
+    return children;
 };
 
 export default PrivateRoute;
